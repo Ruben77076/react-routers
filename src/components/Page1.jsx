4c318ed6d59d7dddfec850f8ef129dfd3ce4187e
Ruben R <rj@allvalleyservicesca.com>
2022-10-25 11:49:11 -0700

    Initial Routing and State lag

---
+++ src/components/Page1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigate from "./Navigate";
+function PageOne() {
+  return (
+    <React.Fragment>
+      <h1>Page 1</h1>
+      <Navigate></Navigate>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+export default PageOne;
---
+++ src/components/Page3.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigate from "./Navigate";
+function PageThree() {
+  return (
+    <React.Fragment>
+      <h1>Page 3</h1>
+      <Navigate></Navigate>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+export default PageThree;
